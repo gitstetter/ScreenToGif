73a7377b71e375c2003cbe9dfd1aa634425f5406
gitstetter <a.fussstetter@gmx.net>
2021-01-22 18:39:41 +0100

    fixed loops

--- ScreenToGif.py
+++ ScreenToGif.py
@@ -1,17 +1,23 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2 as cv
-import time, pyautogui,keyboard, imageio
+import time, pyautogui, keyboard, imageio
  
-if keyboard.is_pressed('s'):
-    buff = []
-    while True:
-        im = pyautogui.screenshot()
-        img = cv.cvtColor(np.array(im), cv.COLOR_RGB2BGR)
-        buff.append(img)
-        time.sleep(0.1)
-        if keyboard.is_pressed('x'):
-            break
-        
-    gif = imageio.mimsave('screen.gif', buff, 'GIF', duration=0.1)
-
+stop=False
+buff = []
+while True:
+    if keyboard.is_pressed('s'):
+        print('starting')
+        while True:
+            im = pyautogui.screenshot()
+            img = cv.cvtColor(np.array(im), cv.COLOR_RGB2BGR)
+            buff.append(img)
+            time.sleep(0.1)
+            if keyboard.is_pressed('x'):
+                print("stopping...")
+                stop=True
+                break
+    if stop:
+        break
+    
+gif = imageio.mimsave('screen.gif', buff, 'GIF', duration=0.3)
