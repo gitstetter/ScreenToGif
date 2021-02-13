2952bad4fa6728a14141346bbfa6a6a21b43e4e4
gitstetter <a.fussstetter@gmx.net>
2021-02-13 15:44:35 +0100

    added cropping

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ I made this little tool to record your screen and save the result in GIF Format.
 
 It's not rocket science, but hey "a gif says more than million words".
 
+Instructions:
+
+1) Press "F1" to crop you screen by holding your left mouse key and dragging a rectangle
+
+2) Press "F2" to start recording
+
+3) Press "F3" to stop recording
+
 Thanks to [Algorithmen verstehen](https://www.youtube.com/channel/UCswWBF6ZkGnLG3sLRR65xRw "Check this out") for the inspiration!
 
 
--- ScreenToGif.py
+++ ScreenToGif.py
@@ -1,20 +1,46 @@
 import pyautogui
 import keyboard
 import imageio
+from pynput.mouse import Listener
 
+def on_click(x, y, button, pressed):
+    if pressed:
+        global x_start, y_start
+        x_start = x
+        y_start = y
+    if not pressed:
+        global x_end, y_end
+        x_end = x
+        y_end = y
+        return False
+
+
+CROPPED=False
 STOP=False
 Buff = []
 while True:
-    if keyboard.is_pressed('s'):
-        print('starting')
-        while True:
+    if keyboard.is_pressed('f1'):
+        print('cropping enabled')
+        with Listener(on_click=on_click) as listener:
+            listener.join()
+        CROPPED=True
+        print('Cropped Area is:')
+        print(x_start,y_start, x_end, y_end)
+
+    if keyboard.is_pressed('f2'):
+        print('Start recording...')
+        while True:           
             im = pyautogui.screenshot()
+            if CROPPED:
+                im =im.crop(box=(x_start*2,y_start*2, x_end*2, y_end*2))              
             Buff.append(im)
-            if keyboard.is_pressed('q'):
-                print("stopping...")
+            if keyboard.is_pressed('f3'):
+                print("Stopping...")
                 STOP=True
                 break
     if STOP:
+        #print(x_start,y_start, x_end-x_start, y_end-y_start)
+
         break
 
 gif = imageio.mimsave('screen.gif', Buff, 'GIF', duration=0.3)
